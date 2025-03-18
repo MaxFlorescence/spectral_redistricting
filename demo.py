422d0f518513a1332cb40855fea46dc8b0ae9ef9
Nathan Kampbell <46498471+natekamp@users.noreply.github.com>
2025-03-18 11:42:18 -0600

    Add demo script

---
+++ demo.py
@@ -0,0 +1,34 @@
+from redistricting import Redistricting
+
+checkpoint_file = "./output/demo_checkpoint.json"
+unfinished, r = Redistricting.from_checkpoint(checkpoint_file)
+    
+if unfinished:
+    if r is None:
+        # no checkpoint was found, so create the redistricting instance for the first time
+        r = Redistricting(
+            graph = "grid",                             # graph to redistrict
+            k = 10,                                     # number of districts
+            assignment = "row",                         # initial district assignment
+            proposal = "revrecom",                      # proposal function for individual redistricting steps
+            steps = 1000,                               # number of steps to run the chain for
+            step_updaters = ["cut edges"],              # statistics to collect after each step
+            single_updaters = ["population deviation"], # statistics to collect at the start and at the end
+            population_key = "pop",                     # key under which each node's population is stored
+            h = 10,                                     # height of the graph (for grid and triangular graphs)
+            w = 10,                                     # width of the graph (for grid and triangular graphs)
+            graph_name = "10x10 grid graph",            # human-readable name of the graph
+            assignment_name = "horizontal stripes"      # human-readable name of the assignment
+        )
+        
+    r.run(
+        plot_interval = 100,                            # how often to plot the graph (in intervals of steps)
+        interactive_level = "progress",                 # how much information should be printed to the console
+        output_level = "all",                           # how much information should be included in the output file
+        output_parent = "./output/",                    # the directory to write output files and images to
+        description = "grid graph run",                 # human-readable description of this run
+        checkpoint_interval = 250,                      # how often to checkpoint the run (in intervals of steps)
+        checkpoint_dest = checkpoint_file,              # the file to save checkpoints to
+        keep_final_step = False                         # maintain the final step's partition data in the checkpoint file
+    )
+# else: the run associated with this checkpoint already finished
